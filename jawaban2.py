6366a24736d29bfec406510041d2d66e9ad94994
nizar202108010373 <nizaralamsyah21@gmail.com>
2022-11-13 15:26:51 +0700

    jawaban2

---
+++ jawaban2.py
@@ -0,0 +1,11 @@
+#Buatlah program seperti dibawah ini sesuaikan dengan nim dan nama anda
+
+print("============")
+print("Program Quiz")
+print("============")
+
+Nama = input("Masukan Nama :")
+NIM = input("Masukan NIM :")
+
+print("Nama :", Nama)
+print("NIM :", NIM)
